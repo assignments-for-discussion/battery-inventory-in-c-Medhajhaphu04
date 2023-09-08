21da8569fdc45a237e0dde27e24e98ae2e5fc2f2
Medhajhaphu04 <96680179+Medhajhaphu04@users.noreply.github.com>
2023-09-08 20:25:44 +0530

    final bucketer.c
    All different test cases for which the expressions inside the asserts fails and the programme will give error is included along with proper comments.

--- bucketer.c
+++ bucketer.c
@@ -30,9 +30,10 @@ void testBucketingByHealth() {
   assert(counts.exchange == 3);
   assert(counts.failed == 1);
   printf("Done counting test 1:)\n");  // this part of code will run correctly 
-  
+
+  // the below test cases test case 2  to 4 have been added for which the programme will show the error test case 1 will run coorectly without any error
   // 2nd test case
-  const int presentCapacities2[] = {115,118,119,80,95,91,77}; // now here error because of 119 thus healthy=3
+  /*const int presentCapacities2[] = {115,118,119,80,95,91,77}; // now assertion for healthy will become false because of 119 ,healthy=3
   const int numberOfBatteries2 = sizeof(presentCapacities2) / sizeof(presentCapacities2[0]);
    printf("Counting batteries by SoH...test 2\n");
   struct CountsBySoH counts2 = countBatteriesByHealth(presentCapacities2, numberOfBatteries2);
@@ -42,7 +43,7 @@ void testBucketingByHealth() {
   printf("Done counting test 2:)\n"); 
   
    // 3rd test case
-  const int presentCapacities3[] = {115,118,96,80,95,91,77}; // now here error because of 96 exchange=4
+  const int presentCapacities3[] = {115,118,96,80,95,91,77}; // now assertion for exchange will become false error because of 96 ,exchange=4
   const int numberOfBatteries3 = sizeof(presentCapacities3) / sizeof(presentCapacities3[0]);
    printf("Counting batteries by SoH...test 3\n");
   struct CountsBySoH counts3 = countBatteriesByHealth(presentCapacities3, numberOfBatteries3);
@@ -52,14 +53,18 @@ void testBucketingByHealth() {
   printf("Done counting test 3:)\n"); 
   
     // 4th test case
-  const int presentCapacities4[] = {115,118,60,80,95,91,77}; // now here error because of 60 failed=2
+  const int presentCapacities4[] = {115,118,60,80,95,91,77}; // now assertion for failed will become false because of 60 ,failed=2
   const int numberOfBatteries4 = sizeof(presentCapacities4) / sizeof(presentCapacities4[0]);
    printf("Counting batteries by SoH...test 4\n");
   struct CountsBySoH counts4 = countBatteriesByHealth(presentCapacities4, numberOfBatteries4);
   assert(counts4.healthy == 2); 
   assert(counts4.exchange == 3);
   assert(counts4.failed == 1);   // now here error will be shown as failed = 2
-  printf("Done counting test 4:)\n"); 
+  printf("Done counting test 4:)\n"); */
+
+/*thus asserts of only 1st test case is true all the time for other test cases asserts will become false as onr of it will become false and will display error message on console
+ the program will terminate after the expression in the 1st assertion become false in the 2nd test case, other test cases will not be checked and on the console the expression
+ which becomes false is also printed */
 }
 
 int main() {
